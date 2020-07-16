51542e2fb1add635869638a08421d23d5c43d19f
Thomas Tosch <thomastosch@mini-de-antoine.home>
2020-07-16 16:10:35 +0200

    init(skelton project): start including graphql apollo etc

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,11 @@
-require('./bootstrap');
+import Vue from 'vue'
+import App from "@/App.vue";
+import apolloProvider from "@/plugins/Apollo";
+import router from "@/router";
+
+const app = new Vue({
+    router,
+    el: '#app',
+    apolloProvider,
+    render: h => h(App)
+});
